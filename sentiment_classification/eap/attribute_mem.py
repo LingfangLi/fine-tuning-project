9036318b508a115bb3698dd6511e216fd22efa04
Procheta Sen <prochetasen@Prochetas-MacBook-Pro.local>
2025-06-06 11:50:07 +0100

    Updated code

---
+++ sentiment_classification/eap/attribute_mem.py
@@ -0,0 +1,205 @@
+from typing import Callable, List, Union, Optional
+from functools import partial
+
+import torch
+from torch import Tensor
+from transformer_lens import HookedTransformer
+from tqdm import tqdm
+from einops import einsum
+import os
+MODEL_LOAD_PATH=r"D:\fine-tuning-project-local\sentiment_model\original_model"
+from .graph import Graph, InputNode, LogitNode, AttentionNode, MLPNode
+
+import torch
+import torch.nn.functional as F
+
+class GPT2Classifier(torch.nn.Module):
+    def __init__(self, d_model):
+        super().__init__()
+        self.linear = torch.nn.Linear(d_model, 1)
+
+    def forward(self, final_hidden):
+        return self.linear(final_hidden).squeeze(-1)
+
+
+def predict_sentiment(text, model, classifier, tokenizer, max_length=64, threshold=0.5):
+    # Tokenize input
+    enc = tokenizer(
+        text,
+        return_tensors='pt',
+        truncation=True,
+        padding='max_length',
+        max_length=max_length
+    )
+    input_ids = enc['input_ids']
+
+    # Get hidden states from TransformerLens model
+    # with torch.no_grad():
+    logits, cache = model.run_with_cache(input_ids)
+    print(logits)
+    final_hidden = cache["resid_post", -1]  # final residual stream
+    last_token_hidden = final_hidden[:, -1, :]  # final token
+
+    # Classifier output
+    raw_output = classifier(last_token_hidden)
+    prob = torch.sigmoid(raw_output).item()
+    predicted_label = int(prob >= threshold)
+
+    return predicted_label, logits
+
+
+def get_npos_input_lengths(model, inputs):
+    tokenized = model.tokenizer(inputs, padding="max_length", truncation=True, max_length=64, return_tensors="pt")
+    n_pos = tokenized.attention_mask.size(1)
+    input_lengths = 1 + tokenized.attention_mask.sum(1)
+    return n_pos, input_lengths
+
+def make_hooks_and_matrices(model: HookedTransformer, graph: Graph, batch_size:int , n_pos:int, scores):
+    activation_difference = torch.zeros((batch_size, n_pos, graph.n_forward, model.cfg.d_model), device='cpu', dtype=model.cfg.dtype)
+
+    processed_attn_layers = set()
+    fwd_hooks_clean = []
+    fwd_hooks_corrupted = []
+    bwd_hooks = []
+    
+    def activation_hook(index, activations, hook, add:bool=True):
+        acts = activations.detach()
+        if not add:
+            acts = -acts
+        try:
+            activation_difference[:, :, index] += acts
+        except RuntimeError as e:
+            print(hook.name, activation_difference[:, :, index].size(), acts.size())
+            raise e
+    
+    def gradient_hook(fwd_index: Union[slice, int], bwd_index: Union[slice, int], gradients:torch.Tensor, hook):
+        grads = gradients.detach()
+        try:
+            if isinstance(fwd_index, slice):
+                fwd_index = fwd_index.start
+            if grads.ndim == 3:
+                grads = grads.unsqueeze(2)
+            s = einsum(activation_difference[:, :, :fwd_index], grads,'batch pos forward hidden, batch pos backward hidden -> forward backward')
+            s = s.squeeze(1)
+            scores[:fwd_index, bwd_index] += s
+        except RuntimeError as e:
+            print(hook.name, activation_difference.size(), grads.size())
+            raise e
+
+    for name, node in graph.nodes.items():
+        if isinstance(node, AttentionNode):
+            if node.layer in processed_attn_layers:
+                continue
+            else:
+                processed_attn_layers.add(node.layer)
+
+        # exclude logits from forward
+        fwd_index =  graph.forward_index(node)
+        if not isinstance(node, LogitNode):
+            fwd_hooks_corrupted.append((node.out_hook, partial(activation_hook, fwd_index)))
+            fwd_hooks_clean.append((node.out_hook, partial(activation_hook, fwd_index, add=False)))
+        if not isinstance(node, InputNode):
+            if isinstance(node, AttentionNode):
+                for i, letter in enumerate('qkv'):
+                    bwd_index = graph.backward_index(node, qkv=letter)
+                    bwd_hooks.append((node.qkv_inputs[i], partial(gradient_hook, fwd_index, bwd_index)))
+            else:
+                bwd_index = graph.backward_index(node)
+                bwd_hooks.append((node.in_hook, partial(gradient_hook, fwd_index, bwd_index)))
+            
+    return (fwd_hooks_corrupted, fwd_hooks_clean, bwd_hooks), activation_difference
+
+def get_scores(model: HookedTransformer, graph: Graph, dataset, metric: Callable[[Tensor], Tensor]):
+    scores = torch.zeros((graph.n_forward, graph.n_backward), device='cpu', dtype=model.cfg.dtype)    
+    
+    total_items = 0
+    for clean, corrupted, label in tqdm(dataset):
+        batch_size = len(clean)
+        total_items += batch_size
+        n_pos, input_lengths = get_npos_input_lengths(model, clean)
+
+        (fwd_hooks_corrupted, fwd_hooks_clean, bwd_hooks), activation_difference = make_hooks_and_matrices(model, graph, batch_size, n_pos, scores)
+        with model.hooks(fwd_hooks=fwd_hooks_corrupted):
+                corrupted_tokens=model.tokenizer(corrupted, padding="max_length", truncation=True, max_length=64, return_tensors="pt")
+                corrupted_logits = model(corrupted_tokens["input_ids"])
+
+        with model.hooks(fwd_hooks=fwd_hooks_clean, bwd_hooks=bwd_hooks):
+            classifier = GPT2Classifier(d_model=model.cfg.d_model)
+            classifier.load_state_dict(torch.load(os.path.join(MODEL_LOAD_PATH, "classifier.pt")))
+            pred_label, log=predict_sentiment(clean, model, classifier, model.tokenizer)
+            print("pred ", log)
+            pred_label=0
+            print(label[0])
+            metric_value = metric(log,label[0])
+            metric_value.backward()
+
+    
+    scores /= total_items
+
+
+    return scores
+
+def get_scores_ig(model: HookedTransformer, graph: Graph, dataset, metric: Callable[[Tensor], Tensor], steps=30):
+    scores = torch.zeros((graph.n_forward, graph.n_backward), device='cpu', dtype=model.cfg.dtype)    
+    
+    total_items = 0
+    for clean, corrupted, label in tqdm(dataset):
+        batch_size = len(clean)
+        total_items += batch_size
+        n_pos, input_lengths = get_npos_input_lengths(model, clean)
+
+        (fwd_hooks_corrupted, fwd_hooks_clean, bwd_hooks), activation_difference = make_hooks_and_matrices(model, graph, batch_size, n_pos, scores)
+
+        with torch.inference_mode():
+            with model.hooks(fwd_hooks=fwd_hooks_corrupted):
+                _ = model(corrupted)
+
+            input_activations_corrupted = activation_difference[:, :, graph.forward_index(graph.nodes['input'])].clone()
+
+            with model.hooks(fwd_hooks=fwd_hooks_clean):
+                clean_logits = model(clean)
+
+            input_activations_clean = input_activations_corrupted - activation_difference[:, :, graph.forward_index(graph.nodes['input'])]
+
+        def input_interpolation_hook(k: int):
+            def hook_fn(activations, hook):
+                new_input = input_activations_clean + (k / steps) * (input_activations_corrupted - input_activations_clean) 
+                new_input.requires_grad = True 
+                return new_input
+            return hook_fn
+
+        total_steps = 0
+        for step in range(1, steps+1):
+            total_steps += 1
+            with model.hooks(fwd_hooks=[(graph.nodes['input'].out_hook, input_interpolation_hook(step))], bwd_hooks=bwd_hooks):
+                classifier = GPT2Classifier(d_model=model.cfg.d_model)
+                pred_label, log=predict_sentiment(clean, model, classifier, model.tokenizer)
+                metric_value = metric(pred_label,label)
+                metric_value.backward()
+
+    scores /= total_items
+    scores /= total_steps
+
+    return scores
+
+allowed_aggregations = {'sum', 'mean', 'l2'}        
+def attribute(model: HookedTransformer, graph: Graph, dataset, metric: Callable[[Tensor], Tensor], aggregation='sum', integrated_gradients: Optional[int]=None):
+    if aggregation not in allowed_aggregations:
+        raise ValueError(f'aggregation must be in {allowed_aggregations}, but got {aggregation}')
+
+        
+    if integrated_gradients is None:
+        scores = get_scores(model, graph, dataset, metric)
+    else:
+        assert integrated_gradients > 0, f"integrated_gradients gives positive # steps (m), but got {integrated_gradients}"
+        scores = get_scores_ig(model, graph, dataset, metric, steps=integrated_gradients)
+
+        if aggregation == 'mean':
+            scores /= model.cfg.d_model
+        elif aggregation == 'l2':
+            scores = torch.linalg.vector_norm(scores, ord=2, dim=-1)
+        
+    scores = scores.cpu().numpy()
+
+    for edge in tqdm(graph.edges.values(), total=len(graph.edges)):
+        edge.score = scores[graph.forward_index(edge.parent, attn_slice=False), graph.backward_index(edge.child, qkv=edge.qkv, attn_slice=False)]
