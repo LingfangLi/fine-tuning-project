58e4e5d41b2f2906a0e3b2aaf690d3927fefd64a
Procheta Sen <prochetasen@Prochetas-MacBook-Pro.local>
2025-06-09 18:46:18 +0100

    Updated

---
+++ QA/GPT2_Train_Squad.py
@@ -0,0 +1,64 @@
+import re
+import pandas as pd
+import os
+import json
+import torch
+from torch.utils.data import DataLoader, Dataset
+from datasets import load_dataset
+from transformers import GPT2Tokenizer
+from transformer_lens import HookedTransformer, HookedTransformerConfig 
+from transformer_lens.train import HookedTransformerTrainConfig, train
+from tqdm import tqdm
+
+
+model = HookedTransformer.from_pretrained("gpt2-small", device="cuda" if torch.cuda.is_available() else "cpu")
+model.cfg.use_attn_in = True
+model.cfg.use_split_qkv_input = True
+model.cfg.use_attn_result = True
+model.cfg.use_hook_mlp_in = True
+
+raw_data = load_dataset('squad')['train'].select(range(40000))
+device1 = model.cfg.device
+
+from transformers import GPT2Tokenizer
+
+def make_prompt_and_target(text, label,answer):
+    return f"Answer the question from the Given context. Context:{text}. Question:{label}. Answer:{answer} "
+MAX_LENGTH=512
+
+class SentimentDataset(Dataset):
+    def __init__(self, dataset, tokenizer):
+        self.tokens = []
+        for sample in dataset:
+            prompt= make_prompt_and_target(sample["context"], sample["question"], sample["answers"]["text"][0])
+            input_ids = tokenizer(prompt, padding="max_length", truncation=True, max_length=MAX_LENGTH, return_tensors="pt")["input_ids"][0]
+            self.tokens.append({"tokens":input_ids})
+
+    def __len__(self):
+        return len(self.tokens)
+
+    def __getitem__(self, idx):
+        return self.tokens[idx]
+
+
+# Load tokenizer and prepare dataset
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+dataset = SentimentDataset(raw_data, tokenizer)
+dataset = dataset[0: (int)(len(dataset)*0.90)]
+dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+
+#print(dataset.tokens)
+# Load tokenizer and prepare dataset
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+
+
+
+
+cfg=HookedTransformerTrainConfig(num_epochs=10, batch_size=20,save_every=500,warmup_steps=2000,max_grad_norm=1.0, lr=0.001, seed=0, momentum=0.0, weight_decay=0.01, optimizer_name='AdamW', device=device1,save_dir="/home/ubuntu/")
+
+train(model, cfg,dataset)
+import torch
+
+torch.save(model.state_dict(), "model_state_dict.pth")
--- QA/SQUADEval.py
+++ QA/SQUADEval.py
@@ -1,111 +1,78 @@
-import torch
-import json
 import os
-import pandas as pd
-from datasets import load_dataset
+import json
+import torch
 from torch.utils.data import DataLoader, Dataset
+from datasets import load_dataset
 from transformers import GPT2Tokenizer
 from transformer_lens import HookedTransformer, HookedTransformerConfig
-# -------- Configurable Parameters --------
-BATCH_SIZE = 20
-DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+import pandas as pd
 
-# -------- Model and Tokenizer Setup --------
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-tokenizer.pad_token = tokenizer.eos_token
 
-def make_prompt_and_target(text, label):
-    return f"Review: {text}\nSentiment:", f"{label}"
+
+def make_prompt_and_target(text, label,answer):
+    return f"Answer the question from the Given context. Context:{text}. Question:{label}.Answer:"
+MAX_LENGTH=512
 
 class SentimentDataset(Dataset):
-    def __init__(self, dataset):
-        self.data = []
-        self.labels = []
-        for i in range(len(dataset)):
-            label =dataset[i]["label"]
-            prompt, target = make_prompt_and_target(dataset[i]["text"], label)
-            full = prompt 
-            input_ids = tokenizer(full, padding="max_length", truncation=True, max_length=30, return_tensors="pt")["input_ids"][0]
-            print(target)
-            prompt_ids = tokenizer(target, padding="max_length", truncation=True, max_length=1, return_tensors="pt")["input_ids"][0]
-            loss_mask = (prompt_ids == tokenizer.pad_token_id).long()
-            self.data.append((input_ids, prompt_ids))
-            self.labels.append(label)
+    def __init__(self, dataset, tokenizer):
+        self.tokens = []
+        self.label=[]
+        for sample in dataset:
+            prompt= make_prompt_and_target(sample["context"], sample["question"], sample["answers"]["text"][0])
+            input_ids = tokenizer(prompt, padding="max_length", truncation=True, max_length=MAX_LENGTH, return_tensors="pt")["input_ids"][0]
+            self.tokens.append(prompt)
+            self.label.append(sample["answers"]["text"][0])
 
     def __len__(self):
-        return len(self.data)
+        return len(self.tokens)
 
     def __getitem__(self, idx):
-        return self.data[idx]
+        return self.tokens[idx], self.label[idx]
 
-def load_model(model_path=None):
-    if model_path is None:
-        model = HookedTransformer.from_pretrained("gpt2-small", device=DEVICE)
-    else:
-        with open(os.path.join(model_path, "config.json"), "r") as f:
-            config_dict = json.load(f)
-            if isinstance(config_dict.get("dtype"), str):
-                config_dict["dtype"] = eval(config_dict["dtype"])
-        cfg = HookedTransformerConfig.from_dict(config_dict)
-        model = HookedTransformer(cfg)
-        model.load_state_dict(torch.load(os.path.join(model_path, "model_state_dict.pth")))
-        model.to(DEVICE)
-    model.eval()
-    return model
 
-def evaluate_accuracy(model, dataset):
-    loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
-    correct = 0
-    total = 0
-    model.eval()
-    with torch.no_grad():
-        for input_ids, labels in loader:
-            input_ids = input_ids.to(DEVICE)
-            logits = model(input_ids)
-            predictions = torch.argmax(logits, dim=-1)[:, -1]  # last token
-            prediction_val, predictions_indices=torch.topk(logits,dim=-1, k=1)
-            predicted=[]
-            predicted.append(predictions_indices[0][0])
-            predicted.append(predictions_indices[1][0])
-            true_labels = labels.tolist()
-            for i in range(len(predicted)):
-                if predicted[i][0] == true_labels[i][0]:
-                    correct+=1
-            text = tokenizer.decode(predictions_indices[0][0][0])
-            
-            true_labels = labels.tolist()
-            print(true_labels)
-            total += len(labels)
-    return correct / total if total > 0 else 0
+raw_data = load_dataset('squad')['train'].select(range(40000))
 
-# -------- Evaluation Modes --------
-def eval_pretrained_on_test(test_path):
-    raw_data = pd.read_csv(test_path).to_dict("records")
-    test_dataset = SentimentDataset(raw_data)
-    model = load_model(None)
-    return evaluate_accuracy(model, test_dataset)
 
-def eval_finetuned_on_test(model_path, test_path):
-    raw_data = pd.read_csv(test_path).to_dict("records")
-    raw_data=load_dataset('yelp_polarity')['train'].select(range(10))
-    
-    test_dataset = SentimentDataset(raw_data)
-    model = load_model(model_path)
-    return evaluate_accuracy(model, test_dataset)
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+dataset = SentimentDataset(raw_data,tokenizer)
+test_dataset = dataset[(int)(len(dataset)*0.90): len(dataset)]
+dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
 
-def eval_transfer(finetuned_model_path, new_test_path):
-    return eval_finetuned_on_test(finetuned_model_path, new_test_path)
+model1 = HookedTransformer.from_pretrained("gpt2-small", device="cuda" if torch.cuda.is_available() else "cpu")
+cg=model1.cfg.to_dict()
+model = HookedTransformer(cg)
+model.load_state_dict(torch.load("model_500.pt"))
+model.to(model.cfg.device)
 
-if __name__ == "__main__":
-    model_path = "yelp_model/"
-    test_path_1 = "/home/ubuntu/fine-tuning-project/sentiment_classification/data/train_5.csv"
-    test_path_2 = "/home/ubuntu/fine-tuning-project/sentiment_classification/data/yelp_train.csv" #Or can load from huggingface dataset, simply edit all the test functions
 
-    #acc_pre = eval_pretrained_on_test(test_path_1)
-    #print(f"Accuracy (Pretrained GPT2): {acc_pre:.4f}")
 
-    acc_post = eval_finetuned_on_test(model_path, test_path_2)
-    print(f"Accuracy (Finetuned GPT2): {acc_post:.4f}")
-    #
-    #acc_transfer = eval_transfer(model_path, test_path_2)
-    #print(f"Transfer Accuracy (Finetuned on Set1, Tested on Set2): {acc_transfer:.4f}")
+
+import nltk
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+
+smoothing = SmoothingFunction().method1  # To avoid 0 scores on short sentences
+
+
+sum_score=0
+correct=0
+for i in range(10):
+    prompt=test_dataset[0][i]
+    label=test_dataset[1][i]
+    print("Prompt: ",prompt)
+    print("Label: ",label)
+    weights = (0.25, 0.25, 0, 0)
+    x=model.generate(prompt,top_k=50,temperature=1)
+    x=x.replace(prompt,"")
+    print("Output ",x)
+    if x==label:
+        correct=correct+1
+    bleu_score = sentence_bleu([label.lower()], x.lower(), smoothing_function=smoothing)
+    print("Bleu Score", bleu_score)
+    sum_score=sum_score+bleu_score
+
+print(correct)
+weights = (1, 0.75, 0, 0)
+print( sentence_bleu(["The cat is on the mat"], "The cat is on the mat", smoothing_function=smoothing))
+sum_score=sum_score/10
+print(f"BLEU score: {sum_score:.4f}")
