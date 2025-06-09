fcc63bd3672ab9a6dadbfe2b038fc1198ce17f23
Procheta Sen <prochetasen@Prochetas-MacBook-Pro.local>
2025-06-09 23:16:37 +0100

    updated

--- Sentiment/src/Eval/TwitterEval.py
+++ Sentiment/src/Eval/TwitterEval.py
@@ -1,124 +1,66 @@
-import torch
-import json
 import os
-import pandas as pd
-from datasets import load_dataset
+import json
+import torch
 from torch.utils.data import DataLoader, Dataset
+from datasets import load_dataset
 from transformers import GPT2Tokenizer
 from transformer_lens import HookedTransformer, HookedTransformerConfig
-# -------- Configurable Parameters --------
-BATCH_SIZE = 2
-DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+import pandas as pd
 
-# -------- Model and Tokenizer Setup --------
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-tokenizer.pad_token = tokenizer.eos_token
+
+
+MAX_LENGTH=148
+
 
 def make_prompt_and_target(text, label):
-    return f"Review: {text}\nSentiment:", f"{label}"
+    return f"Review: {text}\nSentiment:",label
+
+
 
 class SentimentDataset(Dataset):
-    def __init__(self, dataset):
-        self.data = []
-        self.labels = []
-        for i in range(len(dataset)):
-            label =dataset[i]["label"]
-            prompt, target = make_prompt_and_target(dataset[i]["text"], label)
-            full = prompt 
-            input_ids = tokenizer(full, padding="max_length", truncation=True, max_length=30, return_tensors="pt")["input_ids"][0]
-            prompt_ids = tokenizer(target, padding="max_length", truncation=True, max_length=1, return_tensors="pt")["input_ids"][0]
-            loss_mask = (prompt_ids == tokenizer.pad_token_id).long()
-            self.data.append((input_ids, prompt_ids))
-            self.labels.append(label)
+    def __init__(self, dataset, tokenizer):
+        self.tokens = []
+        self.labels=[]
+        for sample in dataset:
+            prompt= make_prompt_and_target(sample["text"], sample["label"])
+            self.tokens.append(prompt)
+            self.labels.append(sample["label"])
 
     def __len__(self):
-        return len(self.data)
+        return len(self.tokens)
 
     def __getitem__(self, idx):
-        return self.data[idx]
+        return self.tokens[idx], self.labels[idx]
 
-def load_model(model_path=None):
-    if model_path is None:
-        model = HookedTransformer.from_pretrained("gpt2-small", device=DEVICE)
-    else:
-        with open(os.path.join(model_path, "config.json"), "r") as f:
-            config_dict = json.load(f)
-            if isinstance(config_dict.get("dtype"), str):
-                config_dict["dtype"] = eval(config_dict["dtype"])
-        cfg = HookedTransformerConfig.from_dict(config_dict)
-        model = HookedTransformer(cfg)
-        model.load_state_dict(torch.load(os.path.join(model_path, "model_state_dict.pth")))
-        model.to(DEVICE)
-    model.eval()
-    return model
 
-def evaluate_accuracy(model, dataset):
-    loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
-    correct = 0
-    total = 0
-    model.eval()
-    with torch.no_grad():
-        for input_ids, labels in loader:
-            input_ids = input_ids.to(DEVICE)
-            #print(input_ids)
-            logits = model(input_ids)
-            #print(logits)
-            logits = logits[:, 0,:]
-            probs = torch.softmax(logits,dim=-1)
-            #print(torch.argmax(probs[0]))
-            # sample from the distribution
-            idx_next = torch.multinomial(probs, num_samples=1)
-            #print("id ",idx_next)
-            text = tokenizer.decode(idx_next[0])
-            true_labels = labels.tolist()
-            #print("true ",true_labels)
+raw_data = load_dataset('frfede/twitter-sentiment')['train'].select(range(50000))
 
-            _, ii= torch.topk(probs[0], k=1, dim=-1)
-            print(ii)
-            try:
-                _, ii= torch.topk(probs[1], k=1, dim=-1)
-                print(ii)
-            except:
-                c=0
-            for i in range(len(true_labels)):
-                if idx_next[i][0]== true_labels[i][0]:
-                    correct+=1
-            total += len(true_labels)
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+test_dataset = SentimentDataset(raw_data,tokenizer)
+test_dataset=test_dataset[(int)(0.90*len(test_dataset)):len(test_dataset)]
 
-    return correct / total if total > 0 else 0
 
-# -------- Evaluation Modes --------
-def eval_pretrained_on_test(test_path):
-    raw_data = pd.read_csv(test_path).to_dict("records")
-    test_dataset = SentimentDataset(raw_data)
-    model = load_model(None)
-    return evaluate_accuracy(model, test_dataset)
+model1 = HookedTransformer.from_pretrained("gpt2-small")
+cg=model1.cfg.to_dict()
+model = HookedTransformer(cg)
+model.load_state_dict(torch.load("model_800.pt"))
+model.to(model.cfg.device)
 
-def eval_finetuned_on_test(model_path, test_path):
-    raw_data = pd.read_csv(test_path).to_dict("records")
-    #raw_data=load_dataset('yelp_polarity')['train'].select(range(10))
-    
-    test_dataset = SentimentDataset(raw_data)
-    model = load_model(model_path)
-    return evaluate_accuracy(model, test_dataset)
 
-def eval_transfer(finetuned_model_path, new_test_path):
-    return eval_finetuned_on_test(finetuned_model_path, new_test_path)
+count=0
+for i in range(100):
+    prompt=test_dataset[0][i][0]
+    label=test_dataset[0][i][1]
 
-if __name__ == "__main__":
-    model_path = "/home/ubuntu/fine-tuning-project/sentiment_classification/transformerlens_twitter_model/"
-    test_path_1 = "/home/ubuntu/fine-tuning-project/sentiment_classification/data/train_5.csv"
-    test_path_2 = "/home/ubuntu/fine-tuning-project/sentiment_classification/src/twitter_test.csv" #Or can load from huggingface dataset, simply edit all the test functions
+    x=model.generate(prompt,top_k=50,temperature=1.2)
+    x=x.replace(prompt,"")
+    if str(label) in x:
+        count=count+1
+    print("Output ",x)
+    print("Label ",label)
 
-    #acc_pre = eval_pretrained_on_test(test_path_1)
-    #print(f"Accuracy (Pretrained GPT2): {acc_pre:.4f}")
 
-    acc_post = eval_finetuned_on_test(model_path, test_path_2)
-    print(f"Accuracy (Finetuned GPT2): {acc_post:.4f}")
-    #
-    #acc_transfer = eval_transfer(model_path, test_path_2)
-    #print(f"Transfer Accuracy (Finetuned on Set1, Tested on Set2): {acc_transfer:.4f}")
-    raw_data = load_dataset('yelp_polarity')['train'].select(range(5))
-    for i in range(5):
-        print(raw_data[i]["label"])
-    raw_data = raw_data.to_pandas().to_dict("records")
+
+
+print(count)
